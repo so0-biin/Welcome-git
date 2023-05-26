1d42486b252ccd6e6845c2817599f1b5785d633d
rumos <zxcv011114@cau.ac.kr>
2023-05-26 20:49:16 +0900

    committer: yunjin Kim feat: check valid github repo or not(empty, invalid) todo: if it is valid address, check public or private

--- CloneMenu.Designer.cs
+++ CloneMenu.Designer.cs
@@ -40,6 +40,7 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -67,7 +68,7 @@
             this.textBox1.Location = new System.Drawing.Point(15, 45);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(773, 43);
+            this.textBox1.Size = new System.Drawing.Size(592, 43);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
@@ -147,11 +148,22 @@
             this.textBox5.Size = new System.Drawing.Size(776, 61);
             this.textBox5.TabIndex = 11;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(613, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 42);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // CloneMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox4);
@@ -185,5 +197,6 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
     }
 }
--- CloneMenu.cs
+++ CloneMenu.cs
@@ -19,7 +19,10 @@ namespace FileManager
             InitializeComponent();
 
             textBox2.ReadOnly = true;
+            textBox3.ReadOnly = true;
+            textBox4.ReadOnly = true;
             textBox5.ReadOnly = true;
+            button1.Enabled = false;
             label1.Text = "Input Git repository address to CLONE";
             label2.Text = "Destination Path";
             label3.Text = "ID";
@@ -27,6 +30,7 @@ namespace FileManager
             label5.Text = "Check Error or Warning";
             button1.Text = "Clone";
             button2.Text = "Exit";
+            button3.Text = "Check";
             
         }
 
@@ -72,6 +76,38 @@ namespace FileManager
             bool isMatch = System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, pattern);
 
             return isMatch;
+        }
+
+        private bool idCheck()
+        {
+            bool isMatch = false;
+            return isMatch;
+        }
+
+        private bool accessTokenCheck()
+        {
+            bool isMatch = false;   
+            return isMatch;    
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "") // repository input nothing
+            {
+                textBox5.Text = "fatal: You must specify a repository to clone.";
+            }
+            else
+            {
+                string pattern = @"^https:\/\/github\.com\/.+\/.+\.git$"; // valid address check
+                bool isMatch = System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, pattern);
+                if (!isMatch ) { // invalid address
+                    textBox5.Text = "fatal: repository \'" + textBox1.Text + "\'" + " does not exist";
+                }
+                else // valid address, check private or public 
+                {
+
+                }
+            }
         }
     }
 }
