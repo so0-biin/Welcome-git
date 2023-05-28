cca7b2a83eb09d4c5c265ce2a7c9899d0015e43e
rumos <zxcv011114@cau.ac.kr>
2023-05-28 10:58:02 +0900

    committer: yunjin Kim feat: make a button for id, access token input check

--- CloneMenu.Designer.cs
+++ CloneMenu.Designer.cs
@@ -41,6 +41,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -112,13 +113,13 @@
             // 
             this.textBox3.Location = new System.Drawing.Point(12, 249);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(319, 25);
+            this.textBox3.Size = new System.Drawing.Size(190, 25);
             this.textBox3.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(334, 231);
+            this.label4.Location = new System.Drawing.Point(205, 231);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(45, 15);
             this.label4.TabIndex = 8;
@@ -126,7 +127,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(337, 249);
+            this.textBox4.Location = new System.Drawing.Point(208, 249);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(451, 25);
             this.textBox4.TabIndex = 9;
@@ -158,11 +159,21 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(665, 249);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 25);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // CloneMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label5);
@@ -198,5 +209,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
