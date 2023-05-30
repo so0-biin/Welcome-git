70f418adc1ff91d6f71042a8e60ac95d50e5891a
so0-biin <cannybinicindy@gmail.com>
2023-05-30 23:40:13 +0900

    committer: subeen Jeon bug fix: history menu merge 후 버튼 클릭 오류현상 수정

--- HistoryMenu.Designer.cs
+++ HistoryMenu.Designer.cs
@@ -104,6 +104,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -113,6 +114,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);     
             // 
             // statusStrip1
             // 
